04f5452f4c9d676e0c3798c451b0591680be141e
Jacob Muchow <jacobamuchow@gmail.com>
2015-12-09 10:48:53 -0600

    Change link styles, added more info to keg/getAll.php return data

--- api/keg/getAll.php
+++ api/keg/getAll.php
@@ -7,15 +7,19 @@ $query =
     "SELECT 
         keg.id as kegId,
         keg.serialNum,
-        kegorder.id as kegOrderId,
-        kegorder.customerId as customerId,
+        kegOrder.id as kegOrderId,
+        kegOrder.customerId as customerId,
+        kegOrder.brewId as brewId,
+        beer.name as beerName,
         customer.firstName as customerFirstName,
         customer.lastName as customerLastName
     FROM keg
     LEFT OUTER JOIN 
-        (SELECT * FROM kegorder WHERE returned = 0) AS kegorder
-        ON kegorder.kegId=keg.id
-    LEFT OUTER JOIN customer ON kegorder.customerId = customer.id
+        (SELECT * FROM kegOrder WHERE returned = 0) AS kegOrder
+        ON kegOrder.kegId = keg.id
+    LEFT OUTER JOIN customer ON kegOrder.customerId = customer.id
+    LEFT OUTER JOIN brew ON kegOrder.brewId = brew.id
+    LEFT OUTER JOIN beer ON brew.beerId = beer.id
     GROUP BY keg.id";
 
 if(($data = Database::runQuery($query))) {
