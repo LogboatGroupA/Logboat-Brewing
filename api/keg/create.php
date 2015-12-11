80c15395992d51ea2f1fbdcb023c777e6b297383
Seth vonSeggern <savtk5@mail.missouri.edu>
2015-12-11 11:27:49 -0600

    updated sql

--- api/keg/create.php
+++ api/keg/create.php
@@ -5,6 +5,8 @@ require '../tools.php';
 
 try {
     $data = Database::runQuery("INSERT INTO keg (serialNum) VALUES (:serialNum)", array("serialNum" => $_POST['serialNum']));
+    $kegId = Database::runQuery("SELECT id FROM keg WHERE serialNum = :serialNum", array("serialNum" => $_POST['serialNum']));
+    $name = Database::runQuery("INSERT INTO kegorder (customerId,userId,kegId) VALUES(31,:userId,:kegId)",array("userId" => $_POST['userId'], "kedId" => $_POST['kegId']));
     success();
 } catch (PDOException $e) {
     fail("Error in api/update.php: " . $e->getMessage());
