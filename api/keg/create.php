6101ad08de47daa16c1ad947d4dbfad377db4f61
Seth vonSeggern <savtk5@mail.missouri.edu>
2015-12-11 11:37:29 -0600

    Update create.php

--- api/keg/create.php
+++ api/keg/create.php
@@ -6,7 +6,7 @@ require '../tools.php';
 try {
     $data = Database::runQuery("INSERT INTO keg (serialNum) VALUES (:serialNum)", array("serialNum" => $_POST['serialNum']));
     $kegId = Database::runQuery("SELECT id FROM keg WHERE serialNum = :serialNum", array("serialNum" => $_POST['serialNum']));
-    $name = Database::runQuery("INSERT INTO kegorder (customerId,userId,kegId) VALUES(31,:userId,:kegId)",array("userId" => $_POST['userId'], "kegId" => $_POST['kegId']));
+    $name = Database::runQuery("INSERT INTO kegorder (customerId,userId,kegId) VALUES(31,:userId,:kegId)",array("userId" => $_SESSION['userId'], "kegId" => $_POST['kegId']));
     success();
 } catch (PDOException $e) {
     fail("Error in api/update.php: " . $e->getMessage());
