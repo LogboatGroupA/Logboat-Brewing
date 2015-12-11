94efbe3318edad43612992054daee1913d6cfc1f
Seth vonSeggern <savtk5@mail.missouri.edu>
2015-12-11 11:50:27 -0600

    Update create.php

--- api/keg/create.php
+++ api/keg/create.php
@@ -5,6 +5,7 @@ require '../tools.php';
 
 try {
     $data = Database::runQuery("INSERT INTO keg (serialNum) VALUES (:serialNum)", array("serialNum" => $_POST['serialNum']));
+    // attempted to insert keg with no user
     $kegId = Database::runQuery("SELECT id FROM keg WHERE serialNum = :serialNum", array("serialNum" => $_POST['serialNum']));
     $theKeg = kegId[0];
     $name = Database::runQuery("INSERT INTO kegorder (customerId,userId,kegId) VALUES(:customerId,:userId,:kegId)",array("customerId" => 31, "userId" => $_SESSION['userId'], "kegId" => $theKeg);
